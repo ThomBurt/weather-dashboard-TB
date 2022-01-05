cb5fdbb489df43c31a6b00cbce6b3a97cfd0dd6f
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-01-05 13:59:41 -0500

    search is working in console.log

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-var searchButtonEl = document.querySelector('#search-btn');
+var searchButtonEl = document.querySelector("#search-btn");
+var cityChoiceEl = document.querySelector("#city-choice");
+var cityNameUI = document.querySelector("#city-name-ui")
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+      // get value from input element
+  var city = cityChoiceEl.value.trim();
+
+  if (city) {
+    getWeather(city);
+
+    // clear old content
+    cityNameUI.textContent = '';
+    cityChoiceEl.value = '';
+  } else {
+    alert('Please enter a City');
+  }
+};
+
+searchButtonEl.addEventListener('click', formSubmitHandler);
