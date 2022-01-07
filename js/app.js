a9068b87dfef7cfe2cf2ff47389ecd001aebb5e7
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-01-07 16:06:01 -0500

    build forecast, watch youtube vid

--- js/app.js
+++ js/app.js
@@ -7,6 +7,16 @@ var windEl = document.querySelector("#wind");
 var humidityEl = document.querySelector("#humidity");
 var uvEl = document.querySelector("#uv");
 
+var forecastDateOne = document.querySelector("#date01");
+var forecastTempOne = document.querySelector("#Temp01");
+var forecastWindOne = document.querySelector("#wind01");
+var forecastHumidityOne = document.querySelector("#humidity01");
+
+var forecastDateTwo = document.querySelector("#date02");
+var forecastTempTwo = document.querySelector("#temp02");
+var forecastWindTwo = document.querySelector("#wind02");
+var forecastHumidityTwo = document.querySelector("#humidity02");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -15,11 +25,12 @@ var formSubmitHandler = function(event) {
 
   if (city) {
     getWeather(city);
+    fiveDayForecast(city);
 
    
     cityNameUI.textContent = '';
     cityChoiceEl.value = '';
-    //displayWeather();
+
     
   } else {
     alert('Please enter a City');
--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,6 @@ function getWeather()  {
     });    
 };
 
-
 var displayWeather = function (weather, city) {
     console.log(weather);
     console.log(city);
@@ -31,7 +30,7 @@ var displayWeather = function (weather, city) {
     cityNameUI.appendChild(actualDate);
     //console.log(newDate);
 
-    
+
  
     tempEl.textContent = "Temp: " + weather.main.temp;
     windEl.textContent = "Wind: " + weather.wind.speed;
@@ -57,6 +56,84 @@ var displayWeather = function (weather, city) {
      // }
 };
 
+function fiveDayForecast() {
+  var city = cityChoiceEl.value.trim();
+  var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=3fed7a6ebcb4e1b063e09df15c8e9e7c&units=imperial&cnt=5';
+  fetch(forecastAPI).then(function(response) {
+    response.json().then(function(data) {
+      displayForecast(data, city);
+    });
+  });    
+};
 
+function displayForecast(weather, city) {
+  console.log(weather);
+
+  //FORECAST DAY 1
+      //Date Day One
+      let unixTimestamp = weather.list[0].dt;
+      var newDate = new Date(unixTimestamp * 1000);
+      var actualDate = document.createElement('p');
+      actualDate.textContent = newDate;
+      forecastDateOne.appendChild(actualDate);
+
+      //Temp Day One
+      TempOne = document.createElement('p');
+      TempOne.textContent = weather.list[0].main.temp;
+      forecastTempOne.appendChild(TempOne);
+
+      // Wind Day One
+      windOne = document.createElement('p');
+      windOne.textContent = weather.list[0].wind.gust;
+      forecastWindOne.appendChild(windOne);
+
+      //Humidity Day One
+      humidityOne = document.createElement('p');
+      humidityOne.textContent = weather.list[0].main.humidity;
+      forecastHumidityOne.appendChild(humidityOne);
+
+
+        //FORECAST DAY 2
+      //Date Day One
+      let unixTimestamp02 = weather.list[1].dt;
+      var newDate = new Date(unixTimestamp * 1000);
+      var actualDate = document.createElement('p');
+      actualDate.textContent = newDate;
+      forecastDateTwo.appendChild(actualDate);
+
+      //Temp Day One
+      tempTwo = document.createElement('p');
+      tempTwo.textContent = weather.list[1].main.temp;
+      forecastTempTwo.appendChild(tempTwo);
+
+      // Wind Day One
+      windTwo = document.createElement('p');
+      windTwo.textContent = weather.list[1].wind.gust;
+      forecastWindTwo.appendChild(windTwo);
+
+      //Humidity Day One
+      humidityTwo = document.createElement('p');
+      humidityTwo.textContent = weather.list[1].main.humidity;
+      forecastHumidityTwo.appendChild(humidityTwo);
+
+
+
+
+
+}
+
+
+//api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=3fed7a6ebcb4e1b063e09df15c8e9e7c
 
 // https://api.openweathermap.org/data/2.5/weather?q=london&appid=3fed7a6ebcb4e1b063e09df15c8e9e7c&units=imperial
+// https://api.openweathermap.org/data/2.5/forecast/daily?q=london&appid=3fed7a6ebcb4e1b063e09df15c8e9e7c&units=imperial&cnt=5
+
+
+
+      // let timeStamp01 = weather.list[0].dt;
+      // let dateObj01 = new Date(timeStamp01);
+      // let month = dateObj01.getMonth() + 1;
+      // let year = dateObj01.getFullYear();
+      // let date = dateObj01.getDate();
+
+      // console.log(month, year, date);
